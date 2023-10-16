b753871cec2d5742fbb7691f58eb4239b837c5d0
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2023-10-17 00:49:15 +0300

    Refactor example: periodic spawn

--- examples/periodic_spawn.rs
+++ examples/periodic_spawn.rs
@@ -43,59 +43,88 @@ fn create_app() -> App {
     app
 }
 
-fn create_bubble(
-    pos: Vec2,
-    graph: &mut BubbleGraph,
-    meshes: &mut Assets<Mesh>,
-    materials: &mut Assets<ColorMaterial>,
-) -> (impl Bundle, BubbleId) {
-    let physics = BubblePhysics {
-        vel: Vec2::new(0., 0.),
-    };
-    let id = graph.add_bubble();
-    let bubble = GraphBubble(id);
-    (
-        (
+struct BubbleGraphBuilder<'c, 'me, 'ma, 'g, 'w, 's> {
+    commands: &'c mut Commands<'w, 's>,
+    graph: &'g mut BubbleGraph,
+    meshes: &'me mut Assets<Mesh>,
+    materials: &'ma mut Assets<ColorMaterial>,
+    positions_by_id: HashMap<BubbleId, Vec2>,
+}
+
+impl<'c, 'me, 'ma, 'g, 'w, 's> BubbleGraphBuilder<'c, 'me, 'ma, 'g, 'w, 's> {
+    pub fn new(
+        commands: &'c mut Commands<'w, 's>,
+        graph: &'g mut BubbleGraph,
+        meshes: &'me mut Assets<Mesh>,
+        materials: &'ma mut Assets<ColorMaterial>,
+    ) -> Self {
+        let positions_by_id = HashMap::new();
+        Self::from_positions_by_id(positions_by_id, commands, graph, meshes, materials)
+    }
+
+    pub fn from_positions_by_id(
+        positions_by_id: HashMap<BubbleId, Vec2>,
+        commands: &'c mut Commands<'w, 's>,
+        graph: &'g mut BubbleGraph,
+        meshes: &'me mut Assets<Mesh>,
+        materials: &'ma mut Assets<ColorMaterial>,
+    ) -> Self {
+        Self {
+            commands,
+            graph,
+            meshes,
+            materials,
+            positions_by_id,
+        }
+    }
+
+    pub fn create_bubble(&mut self, pos: Vec2) -> BubbleId {
+        let physics = BubblePhysics {
+            vel: Vec2::new(0., 0.),
+        };
+        let id = self.graph.insert();
+        let bubble = GraphBubble(id);
+        let mesh = self.meshes.add(shape::Circle::new(50.).into()).into();
+        let material = self.materials.add(ColorMaterial::from(Color::PURPLE));
+        let transform =
+            Transform::from_translation(pos.extend(0.)).with_scale(Vec3::new(2., 1., 0.));
+        self.positions_by_id.insert(id, pos);
+        let bundle = (
             MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-                material: materials.add(ColorMaterial::from(Color::PURPLE)),
-                transform: Transform::from_translation(Vec3::new(pos.x, pos.y, 0.))
-                    .with_scale(Vec3::new(2., 1., 0.)),
+                mesh,
+                material,
+                transform,
                 ..default()
             },
             physics,
             bubble,
-        ),
-        id,
-    )
+        );
+        self.commands.spawn(bundle);
+        id
+    }
+
+    pub fn connect(&mut self, left: BubbleId, right: BubbleId) {
+        assert!(self.graph.contains_node(left));
+        assert!(self.graph.contains_node(right));
+
+        self.graph.add_edge(left, right).unwrap();
+
+        let left_pos = self.positions_by_id[&left];
+        let right_pos = self.positions_by_id[&right];
+
+        // Also create visual entity for the connection
+        let line = shapes::Line(left_pos, right_pos);
+        let path = GeometryBuilder::build_as(&line);
+        let shape = ShapeBundle { path, ..default() };
+        let bundle = (
+            shape,
+            Fill::color(Color::CYAN),
+            Stroke::new(Color::BLACK, 10.0),
+            BubbleConnection(left, right),
+        );
+        self.commands.spawn(bundle);
+    }
 }
-
-fn create_connection(
-    bubble: BubbleId,
-    left: Vec2,
-    right: Vec2,
-    graph: &mut BubbleGraph,
-) -> impl Bundle {
-    // TODO: system to update all connections with their bubble parents
-
-    let main = unsafe { MAIN_BUBBLE_ID.as_ref().unwrap() };
-
-    graph.connect(bubble, *main).unwrap();
-
-    // Also create visual entity for the connection
-    let line = shapes::Line(left, right);
-    let path = GeometryBuilder::build_as(&line);
-    let shape = ShapeBundle { path, ..default() };
-    (
-        shape,
-        Fill::color(Color::CYAN),
-        Stroke::new(Color::BLACK, 10.0),
-        BubbleConnection(bubble, *main),
-    )
-}
-
-#[derive(Component)]
-struct MainBubble;
 
 #[derive(Component, PartialEq, Eq, Hash)]
 pub struct GraphBubble(BubbleId);
@@ -113,10 +142,10 @@ impl Default for Countdown {
     }
 }
 
-static mut MAIN_BUBBLE_ID: Option<BubbleId> = None;
-
 #[derive(Component)]
 pub struct BubbleConnection(BubbleId, BubbleId);
+
+static mut MAIN_BUBBLE_ID: Option<BubbleId> = None;
 
 fn setup_system(
     mut graph: ResMut<BubbleGraph>,
@@ -127,31 +156,18 @@ fn setup_system(
     commands.spawn((Camera2dBundle::default(), MainCamera));
 
     // Make a bubble
-    let physics = BubblePhysics {
-        vel: Vec2::new(0., 0.),
-    };
-    let id = graph.add_bubble();
+    let mut graph = BubbleGraphBuilder::new(&mut commands, &mut graph, &mut meshes, &mut materials);
+
+    let id = graph.create_bubble(Vec2::ZERO);
     unsafe {
         let _ = MAIN_BUBBLE_ID.insert(id);
     };
-    let bubble = GraphBubble(id);
-    commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::PURPLE)),
-            transform: Transform::from_translation(Vec3::new(0., 0., 0.))
-                .with_scale(Vec3::new(2., 1., 0.)),
-            ..default()
-        },
-        physics,
-        MainBubble,
-        bubble,
-    ));
 
+    // Spawn zero marker circle
     commands.spawn(MaterialMesh2dBundle {
         mesh: meshes.add(shape::Circle::new(5.).into()).into(),
         material: materials.add(ColorMaterial::from(Color::RED)),
-        transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+        transform: Transform::from_translation(Vec2::ZERO.extend(1.)),
         ..default()
     });
 }
@@ -163,15 +179,27 @@ fn countdown(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    q: Query<(&GraphBubble, &Transform)>,
 ) {
+    let pos_by_id = q
+        .iter()
+        .map(|(id, tfm)| (id.0, tfm.translation.truncate()))
+        .collect::<HashMap<BubbleId, Vec2>>();
+    let mut builder = BubbleGraphBuilder::from_positions_by_id(
+        pos_by_id,
+        &mut commands,
+        &mut graph,
+        &mut meshes,
+        &mut materials,
+    );
+
     if countdown.timer.tick(time.delta()).just_finished() {
-        // TODO: use better numbers
-        let left = Vec2::new(0., 0.);
         let mut rng = rand::thread_rng();
-        let right = Vec2::new(0. + rng.gen::<f32>() * 20., -150.);
-        let (bubble, id) = create_bubble(right, &mut graph, &mut meshes, &mut materials);
-        commands.spawn(bubble);
-        commands.spawn(create_connection(id, left, right, &mut graph));
+        let pos = Vec2::new(0. + rng.gen::<f32>() * 20., -150.);
+        let id = builder.create_bubble(pos);
+        if let Some(prime) = unsafe { MAIN_BUBBLE_ID } {
+            builder.connect(prime, id);
+        }
     }
 }
 
